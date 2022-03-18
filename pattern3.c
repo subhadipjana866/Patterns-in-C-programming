d9a07a2cf905ff473d6f2bd81aff780dac9384e4
subhadipjana866 <97690258+subhadipjana866@users.noreply.github.com>
2022-03-18 11:20:56 +0530

    Update pattern3.c

--- pattern3.c
+++ pattern3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {
-   int i, space, rows, k = 0;
+   int i, space, rows, j = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i, k = 0) {
-      for (space = 1; space <= rows - i; ++space) {
+   for (i = 1; i <= rows; i++, j = 0) {
+      for (space = 1; space <= rows - i; space++) {
          printf("  ");
       }
-      while (k != 2 * i - 1) {
+      while (j != 2 * i - 1) {
          printf("* ");
-         ++k;
+         j++;
       }
       printf("\n");
    }
